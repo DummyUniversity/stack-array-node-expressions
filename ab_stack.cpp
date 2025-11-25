f8d2d5195513991ed85bf5cd2661e8a24e53a741
stmbsnaasmf <43440784+stmbsnaasmf@users.noreply.github.com>
2025-11-25 10:03:47 +0500

    Update ab_stack.cpp

--- ab_stack.cpp
+++ ab_stack.cpp
@@ -8,7 +8,6 @@ ArrayBasedStack::~ArrayBasedStack() {}
 void ArrayBasedStack::push(const char c) {}
 void ArrayBasedStack::push(const char* str) {}
 bool ArrayBasedStack::pop() { return true; }
-char ArrayBasedStack::getTop() const { char* a; return a; }
+char* ArrayBasedStack::getTop() const { char* a; return a; }
 bool ArrayBasedStack::isEmpty() const { return true; }
 int ArrayBasedStack::getSize() const { return 0; }
-
