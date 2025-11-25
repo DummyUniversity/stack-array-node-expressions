fa3eb29a0e3d90ad7be5e91b7afc50ddfd0791f6
stmbsnaasmf <43440784+stmbsnaasmf@users.noreply.github.com>
2025-11-25 08:33:08 +0500

    Update nb_stack.cpp

--- nb_stack.cpp
+++ nb_stack.cpp
@@ -1,17 +1,11 @@
 #include "nb_stack.h"
 
-Node::Node()
-{
-	next = NULL;
-}
+Node::Node() {}
+Node::~Node() {}
+Node::Node(const char c) {}
+Node::Node(const char* str) {}
 
-Node::Node(char data)
-{
-	this->data = data;
-	next = NULL;
-}
-
-char Node::getData()
+char* Node::getData()
 {
 	return data;
 }
@@ -21,10 +15,9 @@ Node* Node::getNext()
 	return next;
 }
 
-void Node::setData(char data)
-{
-	this->data = data;
-}
+void Node::setData(const char c) {}
+
+void Node::setData(const char* str) {}
 
 void Node::setNext(Node* next)
 {
@@ -37,7 +30,9 @@ NodeBasedStack& NodeBasedStack::operator = (const NodeBasedStack& other) {}
 NodeBasedStack::~NodeBasedStack() {}
 
 void NodeBasedStack::push(const char c) {}
+void NodeBasedStack::push(const char* str) {}
 bool NodeBasedStack::pop() { return true; }
-char NodeBasedStack::getTop() const { return 'a'; }
+char* NodeBasedStack::getTop() const { char* a; return a; }
 bool NodeBasedStack::isEmpty() const { return true; }
 int NodeBasedStack::getSize() const { return 0; }
+
